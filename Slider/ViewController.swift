3ff4bc706ac9ab4d7c21cb7f476069ccab3f1465
cvslel <cenkersyk@gmail.com>
2023-10-05 11:37:41 +0300

    Usage of Slider and SnapKit

--- Slider/ViewController.swift
+++ Slider/ViewController.swift
@@ -10,9 +10,15 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    @objc let slider = UISlider()
+    var sliderValue = 0
+    let sliderValueLabel = UILabel()
+    var image = UIImageView()
+    var images = ["pop1","pop2"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         createUI()
 
     }
@@ -21,19 +27,49 @@ class ViewController: UIViewController {
         
         view.backgroundColor = .white
         
-        let slider = UISlider()
+        slider.value = 0
         slider.minimumValue = 0
+        slider.maximumValue = 100
         slider.minimumTrackTintColor = .green
         slider.maximumTrackTintColor = .red
         slider.thumbTintColor = .blue
+        slider.addTarget(self, action: #selector(sliderMoved), for: UIControl.Event.valueChanged)
         view.addSubview(slider)
         slider.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-90)
             make.left.equalTo(view.snp.left).offset(30)
             make.right.equalTo(view.snp.right).offset(-30)
     }
-        
+        sliderValueLabel.text = "Current Value: "
+        sliderValueLabel.text = String(sliderValue)
+        sliderValueLabel.textAlignment = .center
+        sliderValueLabel.textColor = .black
+        view.addSubview(sliderValueLabel)
+        sliderValueLabel.snp.makeConstraints { make in
+            make.top.equalTo(slider.snp.top).offset(40)
+            make.left.equalTo(view.snp.left).offset(50)
+            make.right.equalTo(view.snp.right).offset(-50)
+            make.height.equalTo(40)
+        }
+        image = UIImageView()
+        view.addSubview(image)
+        image.image = UIImage(named: "pop1")
+        image.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            make.left.equalTo(view.snp.left).offset(5)
+            make.right.equalTo(view.snp.right).offset(-5)
+            make.height.equalTo(400)
+        }
     }
-    
+        
+    @objc func sliderMoved(_ sender: UISlider){
+        sliderValueLabel.text = "Current Value: \(Int(slider.value))"
+        sliderValue = Int(slider.value)
+        print(sliderValue)
+        if sliderValue < 50{
+            image.image = UIImage(named: "pop1")
+        } else if (sliderValue >= 50){
+            image.image = UIImage(named: "pop2")
+        }
+    }
 }
-
